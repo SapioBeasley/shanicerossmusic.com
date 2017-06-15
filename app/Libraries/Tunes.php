1b0081597f5980ae9f18248463e382767d2e4457
Andreas Beasley <andreas@sapioweb.com>
2017-06-15 15:01:18 -0700

    spotify update to need access token

--- app/Libraries/Tunes.php
+++ app/Libraries/Tunes.php
@@ -26,7 +26,24 @@ class Tunes
 
         $client = new \GuzzleHttp\Client();
 
-        $res = $client->request('GET', 'https://api.spotify.com/v1/search?q=' . $term . '&type=track');
+        $base64Creds = base64_encode('ffe7c8521e5c44fc8a65c55fc7dad47e:ec703cb423624acebf4c680d8806b291');
+
+        $token = $client->request('POST', 'https://accounts.spotify.com/api/token', [
+          'headers' => [
+            'Authorization' => 'Basic ' . $base64Creds
+          ],
+          'form_params' => [
+            'grant_type' => 'client_credentials'
+          ]
+        ]);
+
+        $token = json_decode($token->getBody());
+
+        $res = $client->request('GET', 'https://api.spotify.com/v1/search?q=' . $term . '&type=track', [
+          'headers' => [
+            'Authorization' => $token->token_type . ' ' . $token->access_token
+          ],
+        ]);
 
         if ($res->getStatusCode() !== 200) {
             return false;
